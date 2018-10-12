19944cc15e09b53cbc8d689f8d700f1f80710767
Atom Yah <atom@yah.bz>
2018-10-12 16:10:15 +0900

    自動フォロー(旧来のやりかた）console.log(response)

--- server.js
+++ server.js
@@ -111,7 +111,9 @@ app.post('/webhook/twitter', function(req, res) {
       request.post(request_options, (error, response, body) => { console.log(`${response.statusCode} ${response.statusMessage}`); console.log(body) });
       */
       var param = { user_id: follower }
-      twitter.post('friendships/create', param, function(err, tweet, response) {})
+      twitter.post('friendships/create', param, function(err, tweet, response) {
+        console.log(response)
+      })
     }
   }
   /*
